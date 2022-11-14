e22d87fe9516c3750b4e00e6c08eae55f813d95b
SeanEHU <101595096+SeanEHU@users.noreply.github.com>
2022-11-14 11:37:28 +0000

    Initialize
    Created project using Maven.
    Created initial Classes and Interfaces

---
+++ src/main/java/com/mycompany/playlistmanager/IPlaylist.java
@@ -0,0 +1,10 @@
+package com.mycompany.playlistmanager;
+
+interface IPlaylist {
+
+    public String getName();
+    public String getPlaylistID();
+    public boolean addSong();
+    public boolean removeSong();
+    
+}
